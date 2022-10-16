ec500131d55fac1849728c668322e4f82c18b8e6
Henry <henrytnh1110@gmail.com>
2022-10-16 11:30:58 +0700

    Last update

---
+++ src/Pages/History/History.js
@@ -0,0 +1,14 @@
+const History = () => {
+  return (
+    <div>
+      <div className="mt-2 mb-4">
+        <h1 className="text-center text-4xl">History</h1>
+      </div>
+      <div className="mt-10 text-center">
+        <div className="text-lg">There's no histories at the moment</div>
+      </div>
+    </div>
+  );
+};
+
+export default History;
